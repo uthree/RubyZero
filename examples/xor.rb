4e7af9670848808ab8b2f4cd134e79061374cde0
uthree <33905956+uthree@users.noreply.github.com>
2021-07-16 17:09:39 +0900

    update todo.md

---
+++ examples/xor.rb
@@ -0,0 +1,50 @@
+require "../lib/ruby_zero.rb"
+include RubyZero
+
+input = FloatTensor[
+    [0,0],
+    [1,0],
+    [0,1],
+    [1,1]
+]
+
+target = FloatTensor[
+    [0],
+    [1],
+    [1],
+    [0]
+]
+
+
+class TwoLP < NN::Module
+    def initialize(mid_units = 20)
+        @l1 = NN::Linear.new(2,mid_units)
+        @f1 = NN::Sigmoid.new
+        @l2 = NN::Linear.new(mid_units,1)
+        super()
+    end
+    def forward(x)
+        x = @f1.call @l1.call(x)
+        x = @l2.call(x)
+        return x
+    end
+end
+
+model = TwoLP.new(mid_units = 10)
+criterion = Losses::MeanSquaredError.new
+optimizer = Optimizers::SGD.new(learning_rate=0.01)
+optimizer << model
+model.train
+
+output = nil
+
+1000.times do
+    optimizer.zero_grad()
+    output = model.call(input)
+    loss = criterion.call(output,target)
+    loss.backward
+    optimizer.step()
+    p loss.data[0]
+end
+
+p output.data
